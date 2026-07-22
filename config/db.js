d51db803138dbcafb7eef73b5d6a27b6f631f8fc
Sakshi Gawade <sakshigawade2808@gmail.com>
2026-07-22 21:30:24 +0530

    Connected project to Aiven MySQL

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql2");
+const fs = require("fs");
 require("dotenv").config();
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        ca: fs.readFileSync("./ca.pem")
+    }
 });
-
+console.log("Database:", process.env.DB_NAME);
 db.connect((err) => {
     if (err) {
         console.log("Database Connection Failed");
